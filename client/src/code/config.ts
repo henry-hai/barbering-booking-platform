a5db4e4517a56da930ce38bebd291dbffe28fa55
Henry Hai Nguyen <99961130+henry-hai@users.noreply.github.com>
2026-04-02 05:25:08 -0700

    Update userEmail in config to a placeholder

--- client/src/code/config.ts
+++ client/src/code/config.ts
@@ -1,4 +1,4 @@
 export const config: { serverAddress: string, userEmail: string } = {
     serverAddress: "http://localhost:8080",
-    userEmail: "henryhai.nguyen99@gmail.com"
+    userEmail: "your.email@example.com"
   };
